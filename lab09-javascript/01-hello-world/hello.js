11e37923748a219d9174632d006ffa6af61946dd
Tomas Ventrucci <tomas.ventrucci@studio.unibo.it>
2023-12-15 10:00:56 +0100

    javascript

---
+++ lab09-javascript/01-hello-world/hello.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+//span id="ciao"
+
+//costante 
+const tagHello = document.getElementById("ciao");
+//innerHTML è la fnzione che mi fa scrivere del contenuto su html
+tagHello.innerHTML = "Hello World";
